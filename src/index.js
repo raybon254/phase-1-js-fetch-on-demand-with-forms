d8761a9602f008e57052fe876f59c55d5b416989
Brian <musyimib449@gmail.com>
2025-03-19 12:13:10 +0300

    dom manipulation via fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+    const inputForm = document.querySelector("form");
+
+  inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = document.querySelector('#searchByID').value.trim()
+
+    const API_ID = 'http://localhost:3000/movies'
+
+     fetch(`${API_ID}/${input}`)
+     .then((res => res.json()))
+     .then((data => {
+        const title = document.querySelector('#h4')        
+        const summary = document.querySelector('#p')
+
+        title.innerText=`${data.title}`
+        summary.innerText=`${data.summary}`
+     }))
+
+     inputForm.reset();
+  });
 }
 
 document.addEventListener('DOMContentLoaded', init);
